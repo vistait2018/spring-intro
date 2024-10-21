68f4cf9c22e447e995a33ec6a78a860198c7178b
Java20091@ <jidedorcas@gmail.com>
2024-10-21 13:41:47 +0100

    UsingSpringForOurApp

---
+++ src/main/java/com/tutorial/spring/intro/SecondMain.java
@@ -0,0 +1,26 @@
+package com.tutorial.spring.intro;
+
+import com.tutorial.spring.intro.importer.ImporterService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SecondMain {
+    public static void main(String[] args) {
+        var applicationContext =
+                new AnnotationConfigApplicationContext(SpringBeanConfig.class);
+
+        ImporterService importerService =
+                (ImporterService) applicationContext.getBean("json");
+
+        ImporterService importerCsvService =
+                (ImporterService) applicationContext.getBean("csv");
+        importerService.importAll();
+
+        importerCsvService.importAll();
+
+
+
+
+
+
+    }
+}
